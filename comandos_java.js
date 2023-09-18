d779a82ecb60163c4d0a02a4a446db57be213826
lbarradas <142522682+lbarradas@users.noreply.github.com>
2023-09-18 19:06:29 -0300

    Add files via upload

---
+++ comandos_java.js
@@ -0,0 +1,16 @@
+alert("Exibe Alerta na Tela");
+
+document.write("Escreve na Tela");
+// Sinal de + concatena strings (junta)//
+
+Math.round(arredonda valores);
+
+getElementsbyTagName()[];
+getElementbyId();
+getElementsbyClass();
+innetHtml();
+querySelector(); //seleciona via seletor (div.class) ou (div#id)
+
+
+
+
